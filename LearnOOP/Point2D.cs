5909e1db058ce68c37424a1ffc8e73949e60faae
Fortygin <slavafortygin785@gmail.com>
2020-07-22 23:22:43 +0500

    add 8 tasks

--- LearnOOP/Point2D.cs
+++ LearnOOP/Point2D.cs
@@ -6,40 +6,34 @@ using System.Threading.Tasks;
 
 namespace LearnOOP
 {
-    class Point2D
+    public class Point2D
     {
-        private double _x { get; set; }
-        private double _y { get; set; }
+        public double X { get; private set; }
+        public double Y { get; private set; }
 
-
-        public Point2D() { _x = 0; _y = 0; }
+        public Point2D() { X = 0; Y = 0; }
 
         public Point2D(double x, double y)
         {
-            _x = x;
-            _y = y;
-        }
-
-        public double GetDistanceTo(Point2D point1, Point2D point2) 
-        {
-            double result = Math.Sqrt((point1._x - point2._x) * (point1._x - point2._x) + (point1._y - point2._y) * (point1._y - point2._y));
-            return result;
-        }
-
-        public double GetVectorLength(double x1, double y1, double x2, double y2)
-        {
-            double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-
-            return length;
+            X = x;
+            Y = y;
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            return $"({X}, {Y})";
         }
 
-
-
+        public double GetDistanceTo(Point2D point1)
+        {
+            double result = Math.Sqrt(Math.Pow(X - point1.X, 2) + Math.Pow(Y - point1.Y, 2));
+            return result;
+        }
+        public double GetVectorLength()
+        {
+            double length = GetDistanceTo(new Point2D(0, 0));
+            return length;
+        }
 
 
     }
--- LearnOOP/Program.cs
+++ LearnOOP/Program.cs
@@ -8,11 +8,19 @@ namespace LearnOOP
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-
+            
             Point2D point2D = new Point2D();
-
+            Point2D point2D1 = new Point2D(23, 32);
+            Console.WriteLine(point2D.X);
+            Console.WriteLine(point2D1.Y);
+            Console.WriteLine(point2D);
+            Console.WriteLine(point2D1);
+            Console.WriteLine(point2D.GetDistanceTo(point2D));
+            Console.WriteLine(new Point2D(2,0).GetVectorLength());
+            Console.ReadKey();
         }
     }
 }
